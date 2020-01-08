371acea59e4b742e35c0adbfd9eb804021f75c8b
Anup Dey <anupdey99@gmail.com>
2020-01-08 22:19:52 +0600

    CustomSpinnerAdapter

--- CustomSpinnerAdapter.kt
+++ CustomSpinnerAdapter.kt
@@ -6,7 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.TextView
-import com.bd.deliverytiger.app.R
+
 
 class CustomSpinnerAdapter(context: Context, private val resource: Int, private val dataList: MutableList<String>) :
     ArrayAdapter<String>(context, resource, dataList) {
