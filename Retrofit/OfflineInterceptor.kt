55a65d6ea74fb7fa336d8991e546d504c6acd1c4
Anup Dey <anupdey99@gmail.com>
2020-03-03 22:48:14 +0600

    init

---
+++ Retrofit/OfflineInterceptor.kt
@@ -0,0 +1,28 @@
+package com.example.api
+
+import android.content.Context
+import com.example.utils.isConnectedToNetwork
+import okhttp3.CacheControl
+import okhttp3.Interceptor
+import okhttp3.Response
+import java.util.concurrent.TimeUnit
+
+class OfflineInterceptor(private val context: Context): Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        var response = chain.request()
+        if (!context?.isConnectedToNetwork()) {
+            val cacheControl = CacheControl.Builder()
+                .maxStale(7, TimeUnit.DAYS)
+                .build()
+
+            response = response.newBuilder()
+                .removeHeader("Pragma")
+                .removeHeader("Cache-Control")
+                .cacheControl(cacheControl)
+                .build()
+
+        }
+        return chain.proceed(response)
+    }
+}
